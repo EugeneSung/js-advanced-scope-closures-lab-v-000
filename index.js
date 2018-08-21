31b550a696091ae76ff630e6cf666a6a70e938e9
Eugene <yujunsung@gmail.com>
2018-08-21 15:46:34 -0400

    Done.

--- index.js
+++ index.js
@@ -27,3 +27,14 @@ function produceTipCalculator(tipMultiplier){
 
 
 }
+function createDriver(){
+  let DriverId = 0
+  // return the class
+  return class {
+    constructor(name){
+      this.name = name;
+      this.id = ++DriverId;
+    }
+
+  }
+}
